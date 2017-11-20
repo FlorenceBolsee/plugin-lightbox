b779ddf8f38a9e13192624df93becb8f412a555b
Kateryna Voitko <voitko.katerina@gmail.com>
2017-11-20 09:35:14 +0100

    ajout du js et du cross classe css

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-var i = 0;
+$(window).click(function(){
+  $(".popup").fadeOut(400);
+  $("main").removeClass("blur")
+});
+$("section a").click(function(e){
+  e.stopPropagation();
+  $(".popup").fadeIn(500);
+  $("main").addClass("blur");
+});
+$(".close").click(function(){
+  $(".popup").fadeOut(500);
+  $("main").removeClass("blur");
+});
+
+$(document).keyup(function(e){
+  if(e.keyCode == 27){
+    $(".popup").fadeOut(500);
+    $("main").removeClass("blur");
+  }
+
+});
